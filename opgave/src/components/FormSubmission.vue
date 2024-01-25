cf123dc0bb79b56b254762bc86859832cca1a4b0
JairoLopeznavarretePXL <jairo.lopeznavarrete@student.pxl.be>
2024-01-25 20:11:05 +0100

    new

--- opgave/src/components/FormSubmission.vue
+++ opgave/src/components/FormSubmission.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-    <p :class="{ 'green-text': formData.nieuwsbrief, 'red-text': !formData.nieuwsbrief }">
-      {{ formData.nieuwsbrief ? 'Ingeschreven op de nieuwsbrief' : 'Niet ingeschreven op de nieuwsbrief' }}
-    </p>
-    <!-- Andere formulierelementen blijven hetzelfde -->
-  </div>
+  <ul>
+    <li v-for="submission in submissions" :key="submission.id">
+      <h1>Te bekijken film: {{ submission.titel }} - {{submission.score}}</h1>
+      <p>Regisseur: {{ submission.regisseur }}</p>
+      <p>Acteurs: {{ submission.acteurs }}</p>
+      <p>Genre: {{ submission.genre }}</p>
+      <p>Korte inhoud: {{ submission.inhoud }}</p>
+      <p>Toegevoegd op: {{ submission.submissionDate }}</p>
+
+      <!-- Bind class based on nieuwsbrief value -->
+      <p :class="{ 'green-text': submission.nieuwsbrief, 'red-text': !submission.nieuwsbrief }">
+        {{ submission.nieuwsbrief ? 'Ingeschreven op de nieuwsbrief' : 'Niet ingeschreven op de nieuwsbrief' }}
+      </p>
+
+      <!-- Add more details as needed -->
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-/* Andere stijlen blijven hetzelfde */
+<script>
+export default {
+  props: {
+    submissions: Array, // Array of submissions
+  },
+};
+</script>
 
+<style scoped>
 .green-text {
   color: green;
 }
